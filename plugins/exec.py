1e981e1be13cb526531dab0341f2a1be4552ea63
Shantanu Goel <shantanu.goel@gmail.com>
2017-04-02 14:32:27 +0530

    Initial exec plugin implementation

---
+++ plugins/exec.py
@@ -0,0 +1,33 @@
+from subprocess import Popen
+import logging
+
+from config import read_config_plugin
+
+PLUGIN_NAME = 'exec'
+
+
+def exec_notify(filter_name, msg_from, msg_to, msg_subject, msg_content):
+  params = {
+    'cmd': None,
+    'args': [],
+    'env': {
+      'EA_ENV_MSG_FROM': msg_from,
+      'EA_ENV_MSG_TO': msg_to,
+      'EA_ENV_MSG_SUBJECT': msg_subject,
+      'EA_ENV_MSG_CONTENT': msg_content,
+    }
+  }
+
+  plugin_cfg = read_config_plugin(filter_name, PLUGIN_NAME)
+  for key in plugin_cfg.keys():
+    if key == 'env':
+      for env_key in plugin_cfg[key].keys():
+        params[key][env_key] = plugin_cfg[key][env_key]
+    else:
+      params[key] = plugin_cfg[key]
+
+  if not params['cmd']:
+    logging.error('No command specified for plugin %s' % (PLUGIN_NAME))
+    return
+
+  Popen([params['cmd'], params['args']], env=params['env'])
